75c77f67e17c292fbbf7d03f63f12ea64e728adc
gustavo <gustavo@ggpweb.org>
2016-06-15 23:47:04 -0500

    fixing number of parentesis

--- sparktut/sparktut.scala
+++
@@ -1,2 +0,0 @@
-val transac = sc.textFile("/home/gustavo/Documents/transactionsJun2016.csv")
-transac.filter(line => !line.contains("Amount")).map(_.split("\","").map(strings => (strings(5).replaceAll("\"", ""), strings(3).replaceAll("\"", "").toDouble)).reduceByKey((a, b) => a + b).collect() 
